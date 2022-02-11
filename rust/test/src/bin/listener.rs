95222b04efd3562ad5b0d8bb37f8e938559d014f
JavaharlalN <grechanichenkos@yandex.ru>
2022-02-11 17:15:01 +0300

    test

--- rust/test/src/bin/listener.rs
+++ rust/test/src/bin/listener.rs
@@ -1,63 +1,35 @@
-use console::style;
-use lib::utils::parse_float_amount;
-use lib::http_api::HttpApi;
-use lib::{Address, PrivateKey, PublicKey};
-
-use crate::commands::CommandError;
-use crate::common::exit_with_error;
-use crate::common::operation_command::*;
-
 mod common;
-mod trezor;
-mod ledger;
 mod commands;
+use crate::common::exit_with_error;
+use crate::commands::CommandError;
+use crate::common::operation_command::*;
+use lib::Address;
 
-#[derive(Debug)]
-pub struct ParseKeyError {
-    kind: KeyKind,
-    key: String,
-    error: lib::FromPrefixedBase58CheckError,
-}
-
-#[derive(PartialEq, Debug, Clone)]
-pub enum KeyKind {
-    Public,
-    Private,
-}
-
-pub struct TransferLocal {
+pub struct RequestLocal {
     pub verbose: u8,
     pub no_prompt: bool,
     pub endpoint: String,
-    pub from: String
+    pub address: String
 }
 
-impl TransferLocal {
-
+impl RequestLocal {
     pub fn execute(self) -> Result<(), CommandError> {
-        println!("[rust/test/srs/bin/tezedge.rs > execute]");
-        Ok(OperationCommand {
-            options: OperationOptions {
-                no_prompt: self.no_prompt,
-            },
-            api: Box::new(lib::http_api::HttpApi::new(self.endpoint.clone())),
-            // from: public_key.hash().into(),
-            // from: public_key,
-            // fee: self.fee()?,
-            state: Default::default(),
-            trezor_state: None,
-            ledger_state: None,
-            // local_state: Some(LocalWalletState { public_key, private_key }),
-        }.transfer()?)
+        let address = Address::from_base58check(&self.address)
+            .map_err(|err| ParseAddressError {
+                kind: AddressKind::Destination,
+                error: err,
+                address: self.address.clone()
+            }
+        )?;
     }
 }
 
 fn main() {
-    let transfer_obj = TransferLocal {
+    let transfer_obj = RequestLocal {
         verbose: 3,
         no_prompt: false,
         endpoint: "https://rpctest.tzbeta.net".to_string(),
-        from: "tz1WtthyqxFXaC46kBC18UXdqboeTqEjqwtX".to_string(),
+        address: "tz1WtthyqxFXaC46kBC18UXdqboeTqEjqwtX".to_string(),
     };
     let result = transfer_obj.execute();
 
