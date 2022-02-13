2fdb965cfe54c14fde2f1b942fd65cc61ef90310
JavaharlalN <grechanichenkos@yandex.ru>
2022-02-13 15:43:27 +0300

    test

--- rust/listener/src/main.rs
+++ rust/listener/src/main.rs
@@ -1,60 +1,31 @@
-use actix_web::{post, web, HttpResponse, Responder};
-use actix_web::{App, HttpServer, web};
-use log::info;
-use serde::{Serialize, Deserialize};
-use std::process::Command;
-use std::io::{self, Write};
+use actix_web::{rt::System, web, App, HttpResponse, HttpServer};
+use std::sync::mpsc;
+use std::thread;
 
-#[derive(Deserialize)]
-pub struct Request {
-    commands: String,
-}
+#[actix_web::main]
+async fn main() {
+    let (tx, rx) = mpsc::channel();
 
-#[derive(Serialize)]
-pub struct Response {
-    result: bool,
-}
+    thread::spawn(move || {
+        let sys = System::new("http-server");
 
-#[post("/execute")]
-pub async fn execute_command(request: web::Json<Request>) -> impl Responder {
-    info!("validating password for {}", request.commands);
+        let srv = HttpServer::new(|| {
+            App::new().route("/", web::get().to(|| HttpResponse::Ok()))
+        })
+        .bind("127.0.0.1:8080")?
+        .shutdown_timeout(60) // <- Set shutdown timeout to 60 seconds
+        .run();
 
-    let process = Command::new("sh")
-        .arg(&request.commands)
-        .status()
-        .expect("Failed to execute command");
+        let _ = tx.send(srv);
+        sys.run()
+    });
 
-    info!("status: {}", &process.to_string());
+    let srv = rx.recv().unwrap();
 
-    if process.success() {
-        HttpResponse::Ok().json(Response { result: true })
-    } else {
-        HttpResponse::Ok().json(Response { result: false })
-    }
-}
-
-extern crate simple_logger;
-
-mod executor {
-    pub mod validate_password;
-    pub mod execute;
-}
-
-#[actix_rt::main]
-async fn main() -> std::io::Result<()> {
-    simple_logger:: init_with_level(log::Level::Info).unwrap();
-
-    HttpServer::new(|| {
-        App::new()
-            .service(
-                web::scope("/serv/")
-                    .service(executor::validate_password::validate_password)
-                    .service(executor::execute::execute_command)
-            )
-    })
-        .workers(10)
-        .keep_alive(15)
-        .bind("127.0.0.1:8088")?
-        .run()
-        .await
+    // pause accepting new connections
+    srv.pause().await;
+    // resume accepting new connections
+    srv.resume().await;
+    // stop server
+    srv.stop(true).await;
 }
