b4af95b7e4a8441e9895ccf522d15b8b91408d11
root <root@vm3235453.43ssd.had.wf>
2022-02-15 19:35:07 +0200

    minor update

--- test/deploy_call_contracts/get_token_data.ts
+++ test/deploy_call_contracts/get_token_data.ts
@@ -7,11 +7,34 @@ Tezos.addExtension(new Tzip16Module());
 
 async function main() {
 
-const contract = await Tezos.contract.at("KT1DB6jVgAKifGhwtBHzqnNEvtjPM6tJrrx4", tzip16)
+const contract = await Tezos.contract.at("KT1G1vbz9P4R7cPczYTYzM2qf3xFkTLTsbb4", tzip16)
 
 const metadata = await contract.tzip16().getMetadata();
 
 console.log(metadata)
+
+
+const contractAddress = "KT1G1vbz9P4R7cPczYTYzM2qf3xFkTLTsbb4";
+const tokenId = 1;
+
+const rightcontract = await Tezos.contract.at(contractAddress, tzip16)
+.then(contract => {
+  console.log(`Initialising the views for ${contractAddress}...`);
+  return contract.tzip16().metadataViews();
+})
+.then (views => {
+  return views['token_metadata']().executeView(tokenId)
+}).then (result => {
+  console.log('Result of the view token_metadata:');
+  console.log(`name: ${bytes2Char((Object.values(result)[1]).get('name'))}`);
+  console.log(`decimals: ${bytes2Char((Object.values(result)[1]).get('decimals'))}`);
+  console.log(`symbol: ${bytes2Char((Object.values(result)[1]).get('symbol'))}`);
+  console.log(`extra: ${bytes2Char((Object.values(result)[1]).get('extra'))}`);
+})
+.catch(error => console.log(`Error: ${JSON.stringify(error, null, 2)}`));
+
+console.log(rightcontract)
+
 
 }
 
