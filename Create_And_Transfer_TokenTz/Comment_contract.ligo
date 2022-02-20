516f1ecaf31212aaee4bb542e82bb853d25f7d19
Maksik <maksimsedanov65@gmail.com>
2022-02-20 14:36:44 +0300

    Подписывание двух и более транзакций одновременно

--- Create_And_Transfer_TokenTz/Comment_contract.ligo
+++ Create_And_Transfer_TokenTz/Comment_contract.ligo
@@ -1,7 +1,7 @@
-type storage = unit;
+type storage is unit;
 
 type parameter = {
-  tokenContract: contract,
+  tokenContract: address,
   tokenId: nat,
   tokenAmount: nat,
   tokenSender: address,
@@ -9,12 +9,38 @@ type parameter = {
   message: bytes
 };
 
-let main = ([p, s] : [parameter, storage]) : [list <operation>, storage] => {
- // your code
+
+let dest : address = "KT1KR2ft6aRthjkcvTW9FrEPRQoxrfuTpark" as address;
+
+let proxy = ([action, store]: [parameter, storage]): [list<operation>, storage] => {
+  let counter: contract<parameter> =
+    match ((Tezos.get_contract_opt(dest) as option<contract<parameter>>), {
+    Some: (contract: any) => contract,
+    None: () => (failwith("Contract not found.") as contract<parameter>)
+    });
+  /* Reuse the parameter in the subsequent
+     transaction or use another one, `mock_param`. */
+  let mock_param: parameter = transfer([tokenSender, txs=[tokenRecipient,tokenId, tokenAmount]] );
+  let op: operation = Tezos.transaction(action, 0 as tez, counter);
+  return [list([op]), store];
 };
 
 
 
-//ссылки в помощь https://ligolang.org/docs/advanced/entrypoints-contracts/#inter-contract-invocations
-//https://tezostaquito.io/docs/batch_API
-//https://netezos.dev/docs/tezos-forging.html#forge-an-operation-group
+
+
+type comment is michelson_pair(message, "comment")
+type entryAction is
+  | Message of comment
+
+
+function comment(const message : message): return is {message}
+
+
+function main (const p : parameter; const s : storage) : return is
+  block {
+    skip
+  } with case action of
+    | Message(params) -> comment(param)
+    
+  end;
---
+++ Create_And_Transfer_TokenTz/Comment_transaction.ts
@@ -0,0 +1,56 @@
+import { BatchOperation, TezosToolkit } from '@taquito/taquito'
+import { InMemorySigner } from '@taquito/signer'
+const acc = require('./account1.json')
+
+
+export class token_transfer {
+    // настраиваем ссылку на публичный узел тестовой сети
+    private tezos: TezosToolkit
+    rpcUrl: string
+  
+    constructor(rpcUrl: string) {
+      this.tezos = new TezosToolkit(rpcUrl)
+      this.rpcUrl = rpcUrl
+  
+      //считываем почту, пароль и мнемоническую фразу, из которой можно получить приватный ключ
+      this.tezos.setSignerProvider(InMemorySigner.fromFundraiser(acc.email, acc.password, acc.mnemonic.join(' ')))
+    }
+    public async transfer(contract1: string, sender: string, receiver: string, amount: number, id:number ) {
+      
+      const contract = await this.tezos.wallet.at(contract1)
+      const batch = await this.tezos.wallet.batch()
+      .withContractCall(contract.methods.transfer([{
+        from_: sender,
+        txs:[
+          {
+            to_:receiver,
+            token_id: id,
+            amount: amount
+          }
+        ]
+      }]))
+        
+      .withContractCall(contract.methods.mint_more_tokens(1, 4000))
+
+        batch.send()
+
+        const batchOp = await batch.send()
+        console.log("Operation hash:", batchOp.opHash)
+        console.log(`Awaiting for ${batchOp.opHash} to be confirmed...`)
+        return batchOp.confirmation(1).then(() => batchOp.opHash) //ждем одно подтверждение сети
+        .then((hash) => console.log(`Hash: https://hangzhou.tzstats.com/${hash}`)) //получаем хеш операции
+
+        
+    }
+   }
+
+   const RPC_URL = 'https://rpc.hangzhounet.teztnets.xyz'
+const CONTRACT = 'KT1KR2ft6aRthjkcvTW9FrEPRQoxrfuTpark' //адрес опубликованного контракта
+const SENDER = 'tz1Nt3vKhbZpVdCrqgxR9sZDFqUty2h7SMRM' //публичный адрес отправителя — возьмите его из accaunt1.json
+const RECEIVER = 'tz1LiBrF9gibgH5Lf6a7gDjoUfSEg6nxPKsz' //публичный адрес получателя — возьмите его из кошелька Tezos, который вы создали
+const AMOUNT = 12 //количество токенов для отправки. Можете ввести другое число
+const ID=1
+
+new token_transfer(RPC_URL).transfer(CONTRACT, SENDER, RECEIVER, AMOUNT, ID)
+  
+
