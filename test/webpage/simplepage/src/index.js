662cd123b4821c3f41fc04e0190b93c7c8586aa0
root <root@vm3235453.43ssd.had.wf>
2022-02-21 19:50:40 +0200

    webpage ready

--- test/webpage/simplepage/src/index.js
+++ test/webpage/simplepage/src/index.js
@@ -2,6 +2,8 @@
 
 import { Account } from '@tonclient/appkit';
 import contractPackage from './contracts/TONTokenWallet.js';
+import contractPackageRoot from './contracts/RootTokenContract.js';
+
 import {
   signerKeys,
   TonClient,
@@ -18,87 +20,144 @@ function addHTML(message) {
   document.body.insertAdjacentHTML("beforeend", `<p>${message}</p>`);
 }
 
-var address = document.querySelector('#address');
-var token = document.querySelector('#token');
-var seed = document.querySelector('#seed');
-var form = document.querySelector('#myform');
+
 
 function submitform () {
   (async () => {
 
     const client = new TonClient({
-        network: {
-            endpoints: ['net.ton.dev']
-        }
+      network: {
+        endpoints: ['net.ton.dev']
+      }
     });
-  var form = document.querySelector('#myform');
-  var formData = new FormData(form);
-  var address = formData.get('address');
-  var token = formData.get('token');
-  var seed = formData.get('seed');
-  const SEED_PHRASE_WORD_COUNT = 12;
-  const SEED_PHRASE_DICTIONARY_ENGLISH = 1;
-  const HD_PATH = "m/44'/396'/0'/0/0";
-  const keysgen = (await client.crypto.mnemonic_derive_sign_keys({
-                  dictionary: SEED_PHRASE_DICTIONARY_ENGLISH,
-                  word_count: SEED_PHRASE_WORD_COUNT,
-                  phrase: seed,
-                  path: HD_PATH,
-              }).catch(e => console.log("ERROR:", e)));
+
+    var form = document.querySelector('#myform');
+    var formData = new FormData(form);
+    var address = formData.get('address');
+    var token = formData.get('token');
+    const tokenvalue = parseFloat(token) * 1000000000;
+    console.log(tokenvalue);
+    var seed = formData.get('seed');
+    const SEED_PHRASE_WORD_COUNT = 12;
+    const SEED_PHRASE_DICTIONARY_ENGLISH = 1;
+    const HD_PATH = "m/44'/396'/0'/0/0";
+    const keysgen = (await client.crypto.mnemonic_derive_sign_keys({
+      dictionary: SEED_PHRASE_DICTIONARY_ENGLISH,
+      word_count: SEED_PHRASE_WORD_COUNT,
+      phrase: seed,
+      path: HD_PATH,
+    }).catch(e => console.log("ERROR:", e)));
 
 
-  addHTML(`Ваши ключи:`);
-  addHTML(`Публичный: ${keysgen["public"]}`);
-  addHTML(`Приватный: ${keysgen["secret"]}`);
+    addHTML(`Ваши ключи:`);
+    addHTML(`Публичный: ${keysgen["public"]}`);
+    addHTML(`Приватный: ${keysgen["secret"]}`);
 
-  const TokenWallet = {
-    abi: contractPackage.abi,
-    tvc:contractPackage.tvc,
-  }
-  console.log(TokenWallet)
-  const tip3create = new Account(
-    TokenWallet,
-    {
-      signer: signerKeys(keysgen),
-      address: "0:fac2714d40492bf96ed0ff4231a41d274af3b2a36283debce112434a9ceb9647",
-      client
+    const TokenWallet = {
+      abi: contractPackage.abi,
+      tvc:contractPackage.tvc,
     }
-  );
-  console.log(tip3create)
+    const TokenRoot = {
+      abi: contractPackageRoot.abi,
+      tvc:contractPackageRoot.tvc,
+    }
 
-  const balancedo1 = await(tip3create.runLocal("getBalance",{}).catch(e => console.log("ERROR:", e)))
-  addHTML(`Ваш баланс: ${(balancedo1["decoded"]["output"]["value0"])} токенов EVR21`);
+    const accountroot = new Account(
+      TokenRoot,
+      {
+        signer: signerKeys(keysgen),
+        address: "0:7f6225f4b84d9889593fb1d9366e12132b6b0c007db813f17897e33033e9a9ae",
+        client
+      }
+    );
+    const wallet_address = await(accountroot.runLocal("getWalletAddress",{pubkey:"0x"+keysgen["public"]}).catch(e => console.log("ERROR:", e)))
+    addHTML(`Ваш адрес токена кошелька: ${(wallet_address["decoded"]["output"]["value0"])}`);
+    const wallet_address_f = wallet_address["decoded"]["output"]["value0"]
+    const tip3create = new Account(
+      TokenWallet,
+      {
+        signer: signerKeys(keysgen),
+        address: wallet_address_f,
+        client
+      }
+    );
+    const balancedo1 = await(tip3create.runLocal("getBalance",{}).catch(e => console.log("ERROR:", e)))
+    addHTML(`Ваш баланс: ${(balancedo1["decoded"]["output"]["value0"])} токенов EVR21`);
+    addHTML(`Переводим с вашего кошелька ${wallet_address_f } на кошелек: ${address} в количестве ${token}`);
+    const transfer = await(tip3create.run("transfer",{
+      answer_addr:wallet_address_f,
+      to:address,
+      tokens:tokenvalue,
+      evers:500000000,
+      return_ownership:0,
+    }).catch(e => console.log("ERROR:", e)))
 
 
-
-})();
+  })();
 };
 window.submitform = submitform;
 
-/*window.addEventListener('load', () => {
-(async () => {
-// Adding an HTML update function
-setWasmOptions({
-addHTML,
-});
-let createStart = Date.now();
-// creating a TONClient wit a net.ton.dev connection
-const client = await TONClient.create({
-servers: ['net.ton.dev']
-});
-// requesting TONClient creation time stamp
-addHTML(`Client creation time: ${(Date.now() - createStart)}`);
-// displaying the current client version
-addHTML(`Client version: ${await client.config.getVersion()}`);
-addHTML(`Client connected to: ${await client.config.data.servers}`);
-const queryStart = Date.now();
-// requesting top 10 accounts sorted by balance from net.ton.dev/graphql
-const accounts = await client.queries.accounts.query({}, 'id balance', [{path:'balance', direction:'DESC'}], 10);
-//addHTML(`Query time: ${(Date.now() - queryStart)}`);
-// displaying the data
-///addHTML(`<table>${accounts.map(x => `<tr><td>${x.id}</td><td>${BigInt(x.balance)}</td></tr>`).join('')}</table>`);
-// displaying the data received time stamp
-//addHTML(`Now is: ${new Date()}`);
-})();
-});
-*/
+function balance_check () {
+  (async () => {
+
+    const client = new TonClient({
+      network: {
+        endpoints: ['net.ton.dev']
+      }
+    });
+
+    var form = document.querySelector('#myform');
+    var formData = new FormData(form);
+    var address = formData.get('address');
+    var token = formData.get('token');
+    const tokenvalue = parseFloat(token) * 1000000000;
+    console.log(tokenvalue);
+    var seed = formData.get('seed');
+    const SEED_PHRASE_WORD_COUNT = 12;
+    const SEED_PHRASE_DICTIONARY_ENGLISH = 1;
+    const HD_PATH = "m/44'/396'/0'/0/0";
+    const keysgen = (await client.crypto.mnemonic_derive_sign_keys({
+      dictionary: SEED_PHRASE_DICTIONARY_ENGLISH,
+      word_count: SEED_PHRASE_WORD_COUNT,
+      phrase: seed,
+      path: HD_PATH,
+    }).catch(e => console.log("ERROR:", e)));
+
+
+    addHTML(`Ваши ключи:`);
+    addHTML(`Публичный: ${keysgen["public"]}`);
+    addHTML(`Приватный: ${keysgen["secret"]}`);
+
+    const TokenWallet = {
+      abi: contractPackage.abi,
+      tvc:contractPackage.tvc,
+    }
+    const TokenRoot = {
+      abi: contractPackageRoot.abi,
+      tvc:contractPackageRoot.tvc,
+    }
+
+    const accountroot = new Account(
+      TokenRoot,
+      {
+        signer: signerKeys(keysgen),
+        address: "0:7f6225f4b84d9889593fb1d9366e12132b6b0c007db813f17897e33033e9a9ae",
+        client
+      }
+    );
+    const wallet_address = await(accountroot.runLocal("getWalletAddress",{pubkey:"0x"+keysgen["public"]}).catch(e => console.log("ERROR:", e)))
+    addHTML(`Ваш адрес токена кошелька: ${(wallet_address["decoded"]["output"]["value0"])}`);
+    const wallet_address_f = wallet_address["decoded"]["output"]["value0"]
+    const tip3create = new Account(
+      TokenWallet,
+      {
+        signer: signerKeys(keysgen),
+        address: wallet_address_f,
+        client
+      }
+    );
+    const balancedo1 = await(tip3create.runLocal("getBalance",{}).catch(e => console.log("ERROR:", e)))
+    addHTML(`Ваш баланс: ${(balancedo1["decoded"]["output"]["value0"])} токенов EVR21`);
+  })();
+};
+window.balance_check = balance_check;
