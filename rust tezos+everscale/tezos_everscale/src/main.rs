8cbb6f2e2bcff515e528a33c190bb58ea6bc518d
JavaharlalNehru <grechanichenkos@yandex.ru>
2022-03-29 01:54:08 +0300

    added tezos listener

--- rust tezos+everscale/tezos_everscale/src/main.rs
+++ rust tezos+everscale/tezos_everscale/src/main.rs
@@ -1,10 +1,13 @@
-use ureq::Agent;
-use json::parse;
-use std::fs;
+mod functions;
 
-fn get() -> json::JsonValue {
+use functions::*;
+use ureq::Agent;
+use std::fs;
+use serde_json::Value;
+
+fn get() -> Value {
     let agent = Agent::new();
-    let path = format!("https://api.hangzhounet.tzkt.io/v1/accounts/{}/operations", parse(fs::read_to_string(PATH)
+    let path = format!("https://api.hangzhounet.tzkt.io/v1/accounts/{}/operations", serde_json::from_str::<Value>(fs::read_to_string(PATH)
         .unwrap()
         .as_str())
         .unwrap()[1]["address"]
@@ -16,17 +19,17 @@ fn get() -> json::JsonValue {
         .unwrap()
         .into_string()
         .unwrap();
-    let res_json = parse(res.as_str()).unwrap();
+    let res_json = serde_json::from_str::<Value>(res.as_str()).unwrap();
     res_json
 }
 
 const PATH: &str = "tezos_accounts.json";
 
 fn main() {
-    let mut last_len = get().len();
+    let mut last_len = get().as_array().unwrap().len();
     loop {
         let res = get();
-        let len = res.len();
+        let len = res.as_array().unwrap().len();
         if len > last_len {
             println!("{:#}", res[0]);
             last_len = len;
