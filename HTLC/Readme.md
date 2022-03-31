fc3dc5b8a504f7b8490e15753f79ed230f99294d
Maksik <maksimsedanov65@gmail.com>
2022-03-31 21:07:43 +0300

    изменил контракт дописал readme

--- HTLC/Readme.md
+++ HTLC/Readme.md
@@ -2,4 +2,6 @@
 пример [тут](/HTLC/Hashlock_contract.pascaligo)  
 
 Вопрос про htlc контракт на stackExchange https://tezos.stackexchange.com/questions/4076/i-dont-understand-how-the-hashlock-contract-works-in-the-ligo-ide   
-
+полезная ссылка: https://medium.com/protofire-blog/enabling-smart-contract-interaction-in-tezos-with-ligo-functions-and-cps-e3ea2aa49336 тут есть примеры некотрых контрактов 
+https://answerbun.com/ пример перевода токенов стандарта FA2.0
+https://tezos.stackexchange.com/questions/2662/tezos-get-entrypoint-opt-in-ligo-issue 
--- HTLC/call_htlc_contract.ts
+++ HTLC/call_htlc_contract.ts
@@ -1,14 +1,34 @@
+
+import { char2Bytes, } from '@taquito/utils';
 import { TezosToolkit } from '@taquito/taquito';
 import { InMemorySigner } from '@taquito/signer';
 
 
-const tezos = new TezosToolkit('https://rpc.hangzhounet.teztnets.xyz');
-const signer = new InMemorySigner('edskRdkUMmmBorjeetbGiU4cjZ1pbewF6ZmBMj7jCEWxA6pmgTooYTCStHZFitsEgnut7V3YpKt8ptgT1hgK5DuLS4baqXHQXj');
-tezos.setSignerProvider()
+async function example() {
+    const provider = 'https://rpc.hangzhounet.teztnets.xyz';
+    const signer: any = new InMemorySigner('edskRdkUMmmBorjeetbGiU4cjZ1pbewF6ZmBMj7jCEWxA6pmgTooYTCStHZFitsEgnut7V3YpKt8ptgT1hgK5DuLS4baqXHQXj');
+        const tezos = new TezosToolkit(provider);
+    tezos.setSignerProvider( signer );
+    try {
 
-const contract = tezos.contract.at('KT1DGu9gLm5MszYh3zf1uVJvJTJnQD7pqBtg')
+        const contract = await tezos.contract.at('KT1DGu9gLm5MszYh3zf1uVJvJTJnQD7pqBtg')
+
+
+
+        const op = await contract.methods.commit(char2Bytes('Hello')).send()
+        console.log((await op.confirmation(1)).toLocaleString)
 
 
 
 
+
+  }
+
+
+     catch (ex) {
+        console.log(ex)
+    }
+}
+
+example();
 
