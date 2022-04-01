016529b44d53c52ab2b787c7291aabbee629823f
Alex Novikov <alex@asnovikov.ru>
2022-03-31 21:05:32 -0300

    added frontend first page;

--- prod/front/src/app/app.module.ts
+++ prod/front/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSliderModule } from '@angular/material/slider';
 import { MainComponent } from './components/main/main.component';
+import { MaterialExampleModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { MainComponent } from './components/main/main.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
+    MaterialExampleModule,
   ],
   providers: [],
   bootstrap: [MainComponent]
