9c5620a1150c5053e398033fccebde14629344f3
Maksik <maksimsedanov65@gmail.com>
2022-04-02 17:14:17 +0300

    добавил комиссию и проверил работу контракта

--- HTLC/call_htlc_contract.ts
+++ HTLC/call_htlc_contract.ts
@@ -6,18 +6,19 @@ import { InMemorySigner } from '@taquito/signer';
 
 async function example() {
     const provider = 'https://rpc.hangzhounet.teztnets.xyz';
-    const signer: any = new InMemorySigner('edskRdkUMmmBorjeetbGiU4cjZ1pbewF6ZmBMj7jCEWxA6pmgTooYTCStHZFitsEgnut7V3YpKt8ptgT1hgK5DuLS4baqXHQXj');
+    //const signer: any = new InMemorySigner('edskRdkUMmmBorjeetbGiU4cjZ1pbewF6ZmBMj7jCEWxA6pmgTooYTCStHZFitsEgnut7V3YpKt8ptgT1hgK5DuLS4baqXHQXj');
+    const signer: any = new InMemorySigner('edskRrZRXU2vgyFgMt94BKY2Fv1bQCFLrgwo2DwseLoYDvpjZeNohKC1afZtRT55NhhLfAj46PGVL1jAy8WEJZ1m4n3F2Kkc7i')
         const tezos = new TezosToolkit(provider);
     tezos.setSignerProvider( signer );
     try {
         const batch = await tezos.contract.batch()
-      //.withTransfer({ to: 'KT1HEmedN3r828CcCa6wzTd4DMUw1XobxAHa', amount: 2 })
+      //.withTransfer({ to: 'KT1K3JWyQgk1RjjxjUe59o6BaTtc42WtCK2r', amount: 2 })
 
-        const contract = await tezos.contract.at('KT1HEmedN3r828CcCa6wzTd4DMUw1XobxAHa')
+        const contract = await tezos.contract.at('KT1K3JWyQgk1RjjxjUe59o6BaTtc42WtCK2r')
       
       batch
-      .withContractCall(contract.methods.openLock('KT1JMWkKAtB8eNMTYSAmkRuS3xjKHdkgTVGW', char2Bytes('Hello')))
-      //.withContractCall(contract.methods.createLockWithCoins(char2Bytes('Hello'),'KT1JMWkKAtB8eNMTYSAmkRuS3xjKHdkgTVGW' ));
+      .withContractCall(contract.methods.openLock('KT1JMWkKAtB8eNMTYSAmkRuS3xjKHdkgTVGW', char2Bytes('Hello1')))
+      //.withContractCall(contract.methods.createLock(char2Bytes('Hello1'),'KT1JMWkKAtB8eNMTYSAmkRuS3xjKHdkgTVGW' ));
     
       
       
