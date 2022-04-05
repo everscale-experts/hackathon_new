f90b3ca2b9ab0515393987490e2c5e0d9982c249
Maksik <maksimsedanov65@gmail.com>
2022-04-05 03:05:49 +0300

    ошибка при чтении точки входа контракиа токена

--- HTLC/call_htlc_contract_with_tokens.ts
+++ HTLC/call_htlc_contract_with_tokens.ts
@@ -5,22 +5,36 @@ import { InMemorySigner } from '@taquito/signer';
 import {sha256} from 'js-sha256'
 
 
+const ID = 1;
+const AMOUNT = 10000000;
+
 async function example() {
     const provider = 'https://hangzhounet.smartpy.io';
     //const signer: any = new InMemorySigner('edskRdkUMmmBorjeetbGiU4cjZ1pbewF6ZmBMj7jCEWxA6pmgTooYTCStHZFitsEgnut7V3YpKt8ptgT1hgK5DuLS4baqXHQXj');
-    const signer: any = new InMemorySigner('edskRrZRXU2vgyFgMt94BKY2Fv1bQCFLrgwo2DwseLoYDvpjZeNohKC1afZtRT55NhhLfAj46PGVL1jAy8WEJZ1m4n3F2Kkc7i')
+    const signer: any = new InMemorySigner('edskRdkUMmmBorjeetbGiU4cjZ1pbewF6ZmBMj7jCEWxA6pmgTooYTCStHZFitsEgnut7V3YpKt8ptgT1hgK5DuLS4baqXHQXj')
         const tezos = new TezosToolkit(provider);
     tezos.setSignerProvider( signer );
     try {
         const batch = await tezos.contract.batch()
-      // .withTransfer({ to: 'KT1QsSi4rN5EPrE9SsMjUg3XwLFL3rgRTJ9H', amount: 2 })
+        const contract_tokens = await tezos.contract.at('KT1X7iHDQB6iQvejCkwVqE2Nk78fdrcbw66s')
+        // batch
+        // .withContractCall(contract_tokens.methods.transfer([{
+        //     from_:'tz1i5w4BTmwB51efYjcziq6G5eJC5ra2gqHF' ,
+        //     txs:[
+        //       {
+        //         to_:'KT1SuD3WyMK6MYHYqor8pfosUfr5E1LhXxXs',
+        //         token_id: ID,
+        //         amount: AMOUNT,
+        //       }
+        //     ]
+        //   }]))
 
-        const contract = await tezos.contract.at('KT1QsSi4rN5EPrE9SsMjUg3XwLFL3rgRTJ9H')
+        const contract = await tezos.contract.at('KT1SuD3WyMK6MYHYqor8pfosUfr5E1LhXxXs')
       
       batch
-      // .withContractCall(contract.methods.openLock('KT1JMWkKAtB8eNMTYSAmkRuS3xjKHdkgTVGW', char2Bytes('')))
+      .withContractCall(contract.methods.openLock('KT1JMWkKAtB8eNMTYSAmkRuS3xjKHdkgTVGW', char2Bytes('')))
       
-      .withContractCall(contract.methods.createLock(sha256(char2Bytes('')),'KT1JMWkKAtB8eNMTYSAmkRuS3xjKHdkgTVGW' ));
+    //   .withContractCall(contract.methods.createLock(ID, AMOUNT, sha256(char2Bytes('')),'KT1JMWkKAtB8eNMTYSAmkRuS3xjKHdkgTVGW' ));
     
       
       
