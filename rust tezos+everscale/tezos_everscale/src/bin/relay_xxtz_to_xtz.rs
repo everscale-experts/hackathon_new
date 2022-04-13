e479c6e24fc197d5a15d8f01c3763fc4fbcb0125
JavaharlalNehru <grechanichenkos@yandex.ru>
2022-04-13 19:29:59 +0300

    закомментировал лишнее

--- rust tezos+everscale/tezos_everscale/src/bin/relay_xxtz_to_xtz.rs
+++ rust tezos+everscale/tezos_everscale/src/bin/relay_xxtz_to_xtz.rs
@@ -171,83 +171,69 @@ async fn main() {
     );
     // create_batch("0xc39b295aef558a41ef416dcc80bc1def91857e7c16cdf4e698cc8df7cb5c6114", "KT1D4Ri8ntL7HLKTK63cyuV7ZAuMthzrSGJN");
 
-    ton_client::net::subscribe_collection(
-        context.clone(),
-        ton_client::net::ParamsOfSubscribeCollection {
-            collection: "transactions".to_owned(),
-            filter: None,
-            result: "id account_addr in_msg in_message { value }".to_owned(),
-        },
-        |result| async {
-            match result {
-                Ok(result) => {
-                    if let Some(address) = result.result["account_addr"].as_str() {
-                        if let Some(v) = result.result["in_message"]["value"].as_str() {
-                            if address == get_json_field("./dependencies/json/config.json", Some("htlc1"), None)
-                                .as_str().unwrap().to_owned() {
-                                println!("{:#}", result.result);
-                                let ton1 = Arc::new(
-                                    ton_client::ClientContext::new(ton_client::ClientConfig {
-                                        network: ton_client::net::NetworkConfig {
-                                            // server_address: Some("cinet.tonlabs.io".to_owned()), // mainnet
-                                            server_address: Some("net.ton.dev".to_string()), // devnet
-                                            ..Default::default()
-                                        },
-                                        ..Default::default()
-                                    })
-                                    .unwrap(),
-                                );
-                                let pair = decode_msg_body_by_id(
-                                    Arc::clone(&ton1),
-                                    result.result["in_msg"].as_str().unwrap(),
-                                    // "388d0d91d23f4dbc97d277496cc0cd70219e646ae18a6c8e38d1ab7a9ce780e8",
-                                    load_abi_json("./dependencies/json/HelloWallet.abi.json").unwrap(),
-                                ).await;
-                                let v_u64 = hex_to_dec(v);
-                                println!("Amount: {}", v_u64);
-                                // let sender = get_json_field("./dependencies/json/tezos_accounts.json", None, Some(2));
-                                // let receiver = get_json_field("./dependencies/json/tezos_accounts.json", None, Some(3));
-                                println!("destination: {}", pair["dest"].as_str().unwrap());
-                                println!("hash: {}", pair["hash"].as_str().unwrap());
-                                create_batch(pair["hash"].as_str().unwrap(), pair["dest"].as_str().unwrap());
-                                // tezos_transfer(
-                                //     sender["address"].as_str().unwrap(),
-                                //     std::str::from_utf8(
-                                //         hex::decode(
-                                //             address_hex["comment"]
-                                //                 .as_str()
-                                //                 .unwrap()
-                                //         ).unwrap()
-                                //         .as_ref()
-                                //     ).unwrap(),
-                                //     sender["public"].as_str().unwrap(),
-                                //     sender["secret"].as_str().unwrap(),
-                                //     format!("{}", v_u64 as f64 / 1000000000.0).as_str(),
-                                // );
-                            }
-                        }
-                    }
-                }
-                Err(err) => {
-                    println!("(Everscale listener) Error: {}", err);
-                }
-            }
-        },
-    ).await.unwrap();
-
-    // loop {
-    //     let res = tezos_get_transactions();
-    //     let len = res.as_array().unwrap().len();
-    //     if len > last_len {
-    //         let (is_batch, group) = check_batch(res[0]["hash"].as_str().unwrap().to_string());
-    //         if is_batch {
-    //             if let Some(ever_receiver) = tezos_parse_comment(group.clone()) {
-    //                 println!("{}", ever_receiver);
-    //                 let amount = res[0]["amount"].as_i64().unwrap();
-    //                 everscale_transaction((amount.max(1000) * 1000).to_string().as_str(), &ton, config.clone(), ever_receiver).await;
+    // ton_client::net::subscribe_collection(
+    //     context.clone(),
+    //     ton_client::net::ParamsOfSubscribeCollection {
+    //         collection: "transactions".to_owned(),
+    //         filter: None,
+    //         result: "id account_addr in_msg in_message { value }".to_owned(),
+    //     },
+    //     |result| async {
+    //         match result {
+    //             Ok(result) => {
+    //                 if let Some(address) = result.result["account_addr"].as_str() {
+    //                     if let Some(v) = result.result["in_message"]["value"].as_str() {
+    //                         if address == get_json_field("./dependencies/json/config.json", Some("htlc1"), None)
+    //                             .as_str().unwrap().to_owned() {
+    //                             println!("{:#}", result.result);
+    //                             let ton1 = Arc::new(
+    //                                 ton_client::ClientContext::new(ton_client::ClientConfig {
+    //                                     network: ton_client::net::NetworkConfig {
+    //                                         // server_address: Some("cinet.tonlabs.io".to_owned()), // mainnet
+    //                                         server_address: Some("net.ton.dev".to_string()), // devnet
+    //                                         ..Default::default()
+    //                                     },
+    //                                     ..Default::default()
+    //                                 })
+    //                                 .unwrap(),
+    //                             );
+    //                             let pair = decode_msg_body_by_id(
+    //                                 Arc::clone(&ton1),
+    //                                 result.result["in_msg"].as_str().unwrap(),
+    //                                 // "388d0d91d23f4dbc97d277496cc0cd70219e646ae18a6c8e38d1ab7a9ce780e8",
+    //                                 load_abi_json("./dependencies/json/HelloWallet.abi.json").unwrap(),
+    //                             ).await;
+    //                             let v_u64 = hex_to_dec(v);
+    //                             println!("Amount: {}", v_u64);
+    //                             // let sender = get_json_field("./dependencies/json/tezos_accounts.json", None, Some(2));
+    //                             // let receiver = get_json_field("./dependencies/json/tezos_accounts.json", None, Some(3));
+    //                             println!("destination: {}", pair["dest"].as_str().unwrap());
+    //                             println!("hash: {}", pair["hash"].as_str().unwrap());
+    //                             create_batch(pair["hash"].as_str().unwrap(), pair["dest"].as_str().unwrap());
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //             Err(err) => {
+    //                 println!("(Everscale listener) Error: {}", err);
     //             }
     //         }
-    //         last_len = len;
-    //     }
-    // }
+    //     },
+    // ).await.unwrap();
+
+    loop {
+        let res = tezos_get_transactions();
+        let len = res.as_array().unwrap().len();
+        if len > last_len {
+            let (is_batch, group) = check_batch(res[0]["hash"].as_str().unwrap().to_string());
+            if is_batch {
+                if let Some(ever_receiver) = tezos_parse_comment(group.clone()) {
+                    println!("{}", ever_receiver);
+                    let amount = res[0]["amount"].as_i64().unwrap();
+                    everscale_transaction((amount.max(1000) * 1000).to_string().as_str(), &ton, config.clone(), ever_receiver).await;
+                }
+            }
+            last_len = len;
+        }
+    }
 }
