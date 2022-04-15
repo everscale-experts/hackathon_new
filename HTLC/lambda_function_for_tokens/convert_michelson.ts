c44d52e64b27bb23c187f06ad93ed45d66b25632
Maksik <maksimsedanov65@gmail.com>
2022-04-15 15:03:04 +0300

    написал код для конвертации в json формат

---
+++ HTLC/lambda_function_for_tokens/convert_michelson.ts
@@ -0,0 +1,34 @@
+import {TezosToolkit, MANAGER_LAMBDA} from '@taquito/taquito';
+import {InMemorySigner} from '@taquito/signer';
+import {char2Bytes} from '@taquito/utils';
+import { Schema } from "@taquito/michelson-encoder";
+import {Parser} from './taquito-michel-codec/src/taquito-michel-codec'
+
+
+const code = `{ DROP ;
+    PUSH address "KT1Exj7HLktuik8JiKtUavzsHB9heThHUQSf" ;
+    CONTRACT %createLock
+      (pair (pair (pair (nat %amount_tokens) (address %dest1))
+                  (pair (bytes %hash1) (nat %id_tokens)))
+            (address %tokenAddress)) ;
+    IF_NONE { PUSH string "Not a entrypoint" ; FAILWITH } {} ;
+    NIL operation ;
+    SWAP ;
+    PUSH mutez 0 ;
+    PUSH address "KT19xGcNnDwB8uYy18k93FjFv9KNDEivbq87" ;
+    PUSH nat 1 ;
+    PUSH bytes 0xff7a7aff ;
+    PAIR ;
+    PUSH address "KT19xGcNnDwB8uYy18k93FjFv9KNDEivbq87" ;
+    PUSH nat 1000 ;
+    PAIR ;
+    PAIR ;
+    PAIR ;
+    TRANSFER_TOKENS ;
+    CONS }`;
+
+const p = new Parser();
+
+const result = p.parseMichelineExpression(code);
+console.log(JSON.stringify(result));
+
