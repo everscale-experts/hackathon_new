40d99546df35361c658af832589247a30747c06a
JavaharlalNehru <grechanichenkos@yandex.ru>
2022-04-22 20:58:07 +0300

    replaced ABI which used for decoding payload

--- rust_everscale_tezos/tezos_everscale/src/bin/relay_wxtz_to_xtz.rs
+++ rust_everscale_tezos/tezos_everscale/src/bin/relay_wxtz_to_xtz.rs
@@ -1,6 +1,5 @@
 use lib::functions::*;
 use lib::tezos_batch::create_batch_with_coins;
-use lib::tezos_batch::create_batch;
 use std::sync::Arc;
 
 #[tokio::main]
@@ -45,17 +44,25 @@ async fn main() {
                                     })
                                     .unwrap(),
                                 );
-                                let pair = decode_msg_body_by_id(
+                                let payload = decode_msg_body_by_id(
                                     Arc::clone(&ton1),
                                     result.result["in_msg"].as_str().unwrap(),
                                     // "388d0d91d23f4dbc97d277496cc0cd70219e646ae18a6c8e38d1ab7a9ce780e8",
-                                    load_abi_json("./dependencies/json/HelloWallet.abi.json").unwrap(),
+                                    load_abi_json("./dependencies/json/transfer.abi.json").unwrap(),
                                 ).await;
                                 let v_u64 = hex_to_dec(v);
                                 println!("Amount: {}", v_u64);
-                                println!("destination: {}", pair["dest"].as_str().unwrap());
-                                println!("hash: {}", pair["hash"].as_str().unwrap());
+                                println!("Payload: {:#}", payload);
+                                let pair: Value = serde_json::from_slice(
+                                    hex::decode(
+                                        payload["comment"]
+                                            .as_str()
+                                            .unwrap()
+                                    ).unwrap().as_ref()
+                                ).unwrap();
+                                println!("Decoded payload: {:#}", pair);
                                 create_batch_with_coins(pair["hash"].as_str().unwrap(), pair["dest"].as_str().unwrap());
+                                println!("Done");
                             }
                         }
                     }
